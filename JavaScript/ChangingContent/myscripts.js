5d2dd73653fe428fc049ec4428dad944365cd695
Jack Pullman <jack2020@hornbeam.rlogin>
2020-09-28 14:23:50 -0400

    fixed attribute selector

--- JavaScript/ChangingContent/myscripts.js
+++ JavaScript/ChangingContent/myscripts.js
@@ -4,15 +4,15 @@ let buttonFunction = function() {
         case 'hello':
             var helloElements = document.getElementsByClassName('hello');
             for (var i = 0; i < helloElements.length; i++) {
-                var currentHelloElemValue = helloElements[i].getAttribute('textContent').substring(13);
-                helloElements[i].setAttribute('textContent', 'Hello Number ' + (currentHelloElemValue * 2));
+                var currentHelloElemValue = helloElements[i].textContent.substring(13);
+                helloElements[i].textContent = 'Hello Number ' + (currentHelloElemValue * 2);
             }
             break;
         case 'goodbye':
             var byeElements = document.getElementsByClassName('goodbye');
             for (var i = 0; i < byeElements.length; i++) {
-                var currentByeElemValue = byeElements[i].getAttribute('textContent').substring(15);
-                byeElements[i].setAttribute('textContent', 'Goodbye Number ' + (currentByeElemValue * 2));
+                var currentByeElemValue = byeElements[i].textContent.substring(15);
+                byeElements[i].textContent = 'Goodbye Number ' + (currentByeElemValue * 2);
             }
             break;
         default:
